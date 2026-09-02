0803d16c239768610b6d9da349447d86872f952f
yena4076 <69683822+yena4076@users.noreply.github.com>
2026-09-02 17:12:04 +0900

    Create data.cs

---
+++ C#/23_FirebaseCRUD/data.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _025_FirebaseCRUD
+{
+    internal class Data
+    {
+        public string Id { get; set; } // 번호 속성
+        public string SId { get; set; } // 학번 속성
+        public string Name { get; set; } // 이름 속성
+        public string Phone { get; set; } // 전화 속성
+    }
+}
